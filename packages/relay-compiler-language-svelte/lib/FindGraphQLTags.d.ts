3c278b882865f58bf7d84e555d745b9f7d3df0af
Jordan Gensler <jgensler@netflix.com>
2020-05-20 14:08:57 -0700

    Add docs

--- packages/relay-compiler-language-svelte/lib/FindGraphQLTags.d.ts
+++
@@ -1,3 +0,0 @@
-import { GraphQLTagFinder } from 'relay-compiler/lib/language/RelayLanguagePluginInterface';
-export declare function createFinder(finder: GraphQLTagFinder): GraphQLTagFinder;
-//# sourceMappingURL=FindGraphQLTags.d.ts.map
--- packages/relay-compiler-language-svelte/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { PluginInterface } from 'relay-compiler/lib/language/RelayLanguagePluginInterface';
-export default function plugin(): PluginInterface;
-//# sourceMappingURL=index.d.ts.map
