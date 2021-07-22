ff9de14747a940a52b6918390f77a784eb618b71
Jordan Gensler <jgensler@netflix.com>
2021-07-22 15:26:49 -0700

    Expose experimental APIs

--- packages/svelte-relay/src/index.ts
+++ packages/svelte-relay/src/index.ts
@@ -4,3 +4,7 @@ export { getFragment, FragmentResult } from './getFragment';
 export { getQuery, QueryResult } from './getQuery';
 export { getMutation, MutationResult } from './getMutation';
 export { getSubscription } from './getSubscription';
+
+// Export experimental APIs
+import * as experiemental from './experimental';
+export { experiemental };
